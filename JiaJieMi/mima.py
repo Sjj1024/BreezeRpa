4914c4fd16523f3d80bc61d5a20ad434432c7dc7
1024小神 <48399687+Sjj1024@users.noreply.github.com>
2022-04-05 17:43:45 +0800

    添加地址导航

--- JiaJieMi/mima.py
+++ JiaJieMi/mima.py
@@ -1,10 +1,11 @@
 from werkzeug.security import generate_password_hash
 
 # 生成密码
-hash = generate_password_hash('xiaobao')
+hash = generate_password_hash('xiaoshen')
+print(hash)
 
 from werkzeug.security import check_password_hash
 
 # 检查密码
 print(check_password_hash(hash, 'ereg'))
-print(check_password_hash(hash, 'xiaobao'))
+print(check_password_hash(hash, 'xiaoshen'))
